0c816903fffbce9a18a2979b88b2cb5675990173
alyhasnain <alyhasnain.syed@gmail.com>
2026-03-01 13:11:35 +0500

    initial commit

---
+++ flask-vercel-app/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/api/hello")
+def hello():
+    return jsonify({"message": "Hello from Flask API 👋"})
+
+@app.route("/api/add-task", methods=["POST"])
+def add_task():
+    data = request.get_json()
+    task = data.get("task")
+    if task:
+        tasks.append(task)
+        return jsonify({"status": "success", "tasks": tasks})
+    return jsonify({"status": "error"}), 400
+
+@app.route("/api/tasks")
+def get_tasks():
+    return jsonify({"tasks": tasks})
+
+# Required for Vercel
+app = app
